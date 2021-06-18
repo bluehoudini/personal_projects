2e94caf67b94eff760d28334ba4c882530f62db9
bluehoudini <powersrevolution@gmail.com>
2021-06-17 19:49:22 -0600

    init

---
+++ lovesweeper/lovesweeper.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+
+class LoveBot():
+    def __init__(self):
+        self.driver = webdriver.Chrome()
+
+    def login():
+        self.driver.get('https://tinder.com/')
